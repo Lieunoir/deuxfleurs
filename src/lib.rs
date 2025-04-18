2786b9bc703a46b0849341caa0ada780991a043a
Lieunoir <lieunoir@rezel.net>
2025-04-18 12:08:34 +0200

    Remove obsolete wasm fixes

--- src/lib.rs
+++ src/lib.rs
@@ -677,30 +677,6 @@ impl State {
                     &view.as_ref().unwrap()
                 };
 
-                //Fix for clearing bug
-                #[cfg(target_arch = "wasm32")]
-                {
-                    encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                        label: Some("Material Render Pass"),
-                        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                            view: self.pbr_renderer.get_albedo_view(),
-                            resolve_target: None,
-                            ops: wgpu::Operations {
-                                load: wgpu::LoadOp::Clear(wgpu::Color {
-                                    r: 0.0,
-                                    g: 0.0,
-                                    b: 0.0,
-                                    a: 0.0,
-                                }),
-                                store: wgpu::StoreOp::Store,
-                            },
-                        })],
-                        depth_stencil_attachment: None,
-                        occlusion_query_set: None,
-                        timestamp_writes: None,
-                    });
-                }
-
                 let mut material_render_pass =
                     encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                         label: Some("Material Render Pass"),
@@ -1105,7 +1081,6 @@ impl<T: FnOnce(&mut State), U: FnMut(&mut egui::Ui, &mut State)> ApplicationHand
                     let empty_func = js_sys::Function::new_no_args("return false;");
                     canvas.set_oncontextmenu(Some(&empty_func));
                     dst.append_child(&canvas).ok()?;
-                    log::warn!("{}", canvas.width());
                     Some(())
                 })
                 .expect("Couldn't append canvas to document body.");
