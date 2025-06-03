c5fd49d43e9637b6b8dc62ed91c564a6610ca1d5
Lieunoir <33102721+Lieunoir@users.noreply.github.com>
2025-06-03 15:38:32 +0000

    Deploying to gh-pages from @ Lieunoir/deuxfleurs@44c97408ccbaacdb458ab614c23810995b05e41d 🚀

--- trait.impl/core/fmt/trait.Write.js
+++ trait.impl/core/fmt/trait.Write.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["arrayvec",[["impl&lt;const CAP: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.86.0/std/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"arrayvec/struct.ArrayString.html\" title=\"struct arrayvec::ArrayString\">ArrayString</a>&lt;CAP&gt;"]]]]);
+    var implementors = Object.fromEntries([["arrayvec",[["impl&lt;const CAP: <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.87.0/std/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"arrayvec/struct.ArrayString.html\" title=\"struct arrayvec::ArrayString\">ArrayString</a>&lt;CAP&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
