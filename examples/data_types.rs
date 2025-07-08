4272227bbbb303b6738630c79bb549e839a6e302
Lieunoir <lieunoir@rezel.net>
2025-07-08 19:27:45 +0200

    Remove test settings from example

--- examples/data_types.rs
+++ examples/data_types.rs
@@ -11,10 +11,7 @@ fn main() {
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub async fn run() {
     let (spot_v, spot_f) = load_mesh("examples/assets/spot.obj").await.unwrap();
-    let mut handle = deuxfleurs::init(Settings {
-        rerender: true,
-        ..Default::default()
-    });
+    let mut handle = deuxfleurs::init(Settings::default());
     let spot_f = match spot_f {
         SurfaceIndices::Triangles(t) => t,
         _ => panic!(),
